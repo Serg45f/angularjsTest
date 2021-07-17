4883b04b3fdee58f6fd651b6476d9ec06be92cff
Serg45f <sgsavch75@gmail.com>
2021-07-17 10:27:37 +0300

    sds

---
+++ AngularjsTest/src/main/java/com/example/angularjstest/AngularjsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.angularjstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularjsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularjsTestApplication.class, args);
+	}
+
+}
---
+++ AngularjsTest/src/test/java/com/example/angularjstest/AngularjsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.angularjstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularjsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
